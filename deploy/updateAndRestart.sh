ceba084855eab0eb60f929f6d4316485a8d16b89
Emmanuel Daniel <ecomje@gmail.com>
2020-02-12 23:10:09 +0100

    .

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -9,7 +9,7 @@ rm -rf /home/ubuntu/authententication/
 GIT_USERNAME=$GIT_USERNAME
 GIT_PASSWORD=$GIT_PASSWORD
 # clone the repo again
-git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@gitlab.com/emmsdan/authententication.git
+git clone https://"$GIT_USERNAME":"$GIT_PASSWORD"@gitlab.com/emmsdan/authententication.git
 
 #source the nvm file. In an non
 #If you are not using nvm, add the actual path like
